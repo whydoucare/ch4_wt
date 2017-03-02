7abb083c97132435ee8f134605aaa9f09c6d9996
Sanjay Akkina <sakkina1@uncc.edu>
2017-03-02 20:41:18 +0000

    edit haml file

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,4 +13,8 @@ class MoviesController < ApplicationController
         @movie = Movie.find(id) # look up movie by unique ID
         # will render app/views/movies/show.html.haml by default
     end
+    
+    def new 
+        # default: render 'new' template
+    end
 end
